7e814c4a676866f28abbfd7b869edc1fd58ba476
Alan Oliveira <72306242+Aln1002@users.noreply.github.com>
2021-07-19 02:18:36 -0300

    Add files via upload

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,79 @@
+import { CadastroCrudComponent } from './views/product-crud/cadastro-crud.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import { MatSidenavModule } from  '@angular/material/sidenav';
+import { MatCardModule } from  '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { CadastroCreateComponent } from './components/cadastro/cadastro-create/cadastro-create.component';
+import { MatButtonModule } from  '@angular/material/button';
+import { MatSnackBarModule } from  '@angular/material/snack-bar';
+import { HttpClientModule } from  '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CadastroReadComponent } from './components/cadastro/cadastro-read/cadastro-read.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { RedDirective } from './directives/red.directive';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from  '@angular/common';
+import { ForDirective } from './directives/for.directive';
+import { CadastroUpdateComponent } from './components/cadastro/cadastro-update/cadastro-update.component';
+import { CadastroDeleteComponent } from './components/cadastro/cadastro-delete/cadastro-delete.component';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    CadastroCreateComponent,
+    CadastroReadComponent,
+    RedDirective,
+    ForDirective,
+    CadastroUpdateComponent,
+    CadastroDeleteComponent,
+    CadastroCrudComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
